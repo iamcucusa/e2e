8db4b6c38ce8f304c295e0794fbf8d231b377862
Grace Henriquez <iamcucusa@gmail.com>
2020-04-13 15:26:01 +0200

    create rule creation feature

---
+++ iDareUI/RuleCreation.feature
@@ -0,0 +1,20 @@
+﻿Feature: RuleCreation
+	In order to create a new footprint in teaching module
+	As an DebugUser iDare teacher 
+	I want to create footprints in teaching module
+
+Background: 
+	Given I navigate successfully to the teaching module
+	And I am logged in as teacher user
+	And I am in the issue list in teaching module
+
+	
+	Scenario: 2052 Create Rule with 2 sequenced events sucessfully
+	Given I click in the issue Issue1 from the issue list
+		And I click add footprint for Issue1
+		And Event Catalog is correct for the system cobas6800/8800
+		And I select Case1 from production
+		And I add the event 'Analytic System Stage Changed to Running' from the catalog to the sequenced events
+		And I add the event 'Analytic Stage Changed to Error' from the catalog to the sequenced events
+	When I save the Rule
+	Then the rule is correct
---
+++ iDareUI/RuleCreationSteps.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace iDareUI
+{
+    [Binding]
+    public class RuleCreationSteps
+    {
+        [Given(@"I click in the issue Issue(.*) from the issue list")]
+        public void GivenIClickInTheIssueIssueFromTheIssueList(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I click add footprint for Issue(.*)")]
+        public void GivenIClickAddFootprintForIssue(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"Event Catalog is correct for the system cobas(.*)")]
+        public void GivenEventCatalogIsCorrectForTheSystemCobas(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I select Case(.*) from production")]
+        public void GivenISelectCaseFromProduction(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I add the event '(.*)' from the catalog to the sequenced events")]
+        public void GivenIAddTheEventFromTheCatalogToTheSequencedEvents(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I save the Rule")]
+        public void WhenISaveTheRule()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the rule is correct")]
+        public void ThenTheRuleIsCorrect()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
